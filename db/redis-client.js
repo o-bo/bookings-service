938410af84c4281efda5301c16575a1f39a4b0c0
Olivier Berho <olivier@kasual.biz>
2021-10-29 10:59:54 +0200

    setup redis-client

--- db/redis-client.js
+++ db/redis-client.js
@@ -2,15 +2,50 @@
 const redis = require('redis');
 const { promisify } = require('util');
 
-const client = redis.createClient(process.env.REDIS_URL || 'localhost:6379');
+const client = redis.createClient();
+client.on('error', (err) => {
+  // console.log('Redis Client Error', err);
+});
+client.on('connect', () => {
+  // console.log('Connected!');
+});
+
+const getAsync = promisify(client.get).bind(client);
+// Set a TTL : client.set(key, value, 'EX', 60 * 60 * 24, callback);
+const setAsync = promisify(client.set).bind(client);
+const delAsync = promisify(client.del).bind(client);
+// NOTE : do not use keys in production :
+// it might block the redis server ; see SCAN documentation instead
+// keysAsync: promisify(client.keys).bind(client),
+
+/**
+ * Build the cache key
+ *
+ * @param {string} namespace - base namespace for the cache key
+ * @param {Object} params - optional params to build the cache key
+ * @returns {string} a string representing the cache key formated using the namespace
+ * and each key/value of the params separated by ':'
+ * Keys from params must be sorted in alphabetical order.
+ *
+ * Example : <namespace>:<k1>:<v1>:<k2>:<v2>
+ */
+const getCacheKey = (namespace, params) => {
+  const paramsEntries = Object.entries(params).sort(([k1], [k2]) => (k1 < k2 ? -1 : 1));
+  const cacheKey = paramsEntries.reduce((acc, [k, v]) => `${acc}:${k}:${v}`, namespace);
+  return cacheKey;
+};
 
 module.exports = {
-  ...client,
-  getAsync: promisify(client.get).bind(client),
-  // Set a TTL : client.set(key, value, 'EX', 60 * 60 * 24, callback);
-  setAsync: promisify(client.set).bind(client),
-  delAsync: promisify(client.del).bind(client),
-  // NOTE : do not use it in production :
-  // it might block the redis server ; see SCAN documentation instead
-  // keysAsync: promisify(client.keys).bind(client),
+  retrieve: (namespace, params) => {
+    const cacheKey = getCacheKey(namespace, params);
+    return getAsync(cacheKey).then((result) => result && JSON.parse(result)).catch(() => null);
+  },
+  cache: async (namespace, params, data) => {
+    const cacheKey = getCacheKey(namespace, params);
+    return setAsync(cacheKey, JSON.stringify(data), 'EX', 2).then(() => 1).catch(() => 0);
+  },
+  delete: (namespace, params) => {
+    const cacheKey = getCacheKey(namespace, params);
+    return delAsync(cacheKey).then(() => 1).catch(() => 0);
+  },
 };
