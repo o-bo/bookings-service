1287b31371483f6a9980008c3fad85510370e4ac
Olivier Berho <olivier@kasual.biz>
2021-10-30 21:44:29 +0200

    more utils

--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,8 @@ const isObject = (o) => Object.prototype.toString.call(o) === '[object Object]';
 
 const toCamel = (s) => s.replace(/([-_][a-z])/gi, ($1) => $1.toUpperCase().replace('-', '').replace('_', ''));
 
+const toSnake = (s) => s.split(/(?=[A-Z])/).join('_').toLowerCase();
+
 const keysToCamel = (o) => {
   if (isObject(o)) {
     const n = {};
@@ -13,16 +15,42 @@ const keysToCamel = (o) => {
     });
 
     return n;
-  } if (isArray(o)) {
+  }
+
+  if (isArray(o)) {
     return o.map((i) => keysToCamel(i));
   }
 
   return o;
 };
 
+const keysToSnake = (o) => {
+  if (isObject(o)) {
+    const n = {};
+
+    Object.keys(o).forEach((k) => {
+      n[toSnake(k)] = keysToSnake(o[k]);
+    });
+
+    return n;
+  }
+
+  if (isArray(o)) {
+    return o.map((i) => keysToSnake(i));
+  }
+
+  return o;
+};
+
+const sleep = (ms) => new Promise((resolve) => {
+  setTimeout(resolve, ms);
+});
+
 module.exports = {
   isObject,
   isArray,
   toCamel,
   keysToCamel,
+  keysToSnake,
+  sleep,
 };
